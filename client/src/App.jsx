96ba3921c75e261c91f9408ed69136be8477a5ea
Create-88 <141834742+Create-88@users.noreply.github.com>
2024-02-24 23:21:17 +1030

    debug navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,11 +7,7 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
-<<<<<<< HEAD
-// import Navigation from './components/Navigation';
-=======
 import Navbar from './components/Navigation';
->>>>>>> 98e891cc1451838850c7b8a4d31933f5cf783e43
 import { VetProvider } from './utils/GlobalState';
 
 const httpLink = createHttpLink({
@@ -38,11 +34,7 @@ function App() {
     <ApolloProvider client={client}>
       <div>
         <VetProvider>
-<<<<<<< HEAD
-          {/* <Navigation /> */}
-=======
           <Navbar />
->>>>>>> 98e891cc1451838850c7b8a4d31933f5cf783e43
           <Outlet />
         </VetProvider>
       </div>
