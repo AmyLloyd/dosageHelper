0c743e92eab9694857c058bb794eb5f80f9e0aff
Amy Lloyd <amylloyd30@gmail.com>
2024-02-27 12:32:32 +1030

    change port to 3001 try to connect heroku WIP

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const db = require('./config/connection');
 
 console.log(db, "db");
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
     typeDefs,
