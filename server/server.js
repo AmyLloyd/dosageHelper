951cdc4ff2c19261c3b979030ebab10e82f5d2b3
Amy Lloyd <amylloyd30@gmail.com>
2024-02-27 10:59:17 +1030

    change port

--- server/server.js
+++ server/server.js
@@ -8,23 +8,27 @@ const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+console.log(db, "db");
+
+const PORT = process.env.PORT || 8080;
 const app = express();
 const server = new ApolloServer({
     typeDefs,
     resolvers
   });
+
+console.log(server, "server");
   
- const startApolloServer = async () => {
-  await server.start();
+const startApolloServer = async () => {
+await server.start();
 
-  app.use(express.urlencoded({ extended: false }));
-  app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-  app.use('/graphql', expressMiddleware(server, {
-    context: authMiddleware
-   
-  }));
+app.use('/graphql', expressMiddleware(server, {
+  context: authMiddleware
+  
+}));
 
 
   if (process.env.NODE_ENV === 'production') {
@@ -34,7 +38,7 @@ const server = new ApolloServer({
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
   }
-
+console.log(app, "app");
 
   db.once("open", () => {
       app.listen(PORT, () => {
