a2cbc8f5e521fe6d9592a5c06a68a89d981ff665
Amy Lloyd <amylloyd30@gmail.com>
2024-06-06 17:46:59 +0930

    activate routes checked

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,12 +1,10 @@
 import SignupForm from '../components/Signup';
 
 function Signup() {
-    
     return (
         <div className='container-a'>
             <SignupForm />
         </div>
     )
 }
-
 export default Signup;
